b3f3d4d91737686c30aa5db1f36e852b9944607b
Ziyi Chen <zc2913@nyu.edu>
2023-11-01 11:20:31 -0400

    hw08

--- sketch.js
+++ sketch.js
@@ -1,23 +1,47 @@
-let mImg;
-let MONDRIAN_BLUE = color(20, 20, 220);
+let originalImg;
+let replaceColorsButton;
+let randomizeColorsButton;
+
 
 function preload() {
-  mImg = loadImage("Mondriaan.jpeg"); // Replace with the path to your image
+  originalImg = loadImage("Mondriaan.jpeg"); // Replace with the path to your image
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   pixelDensity(1);
-  mImg.resize(0, height);
+  originalImg.resize(0, height);
+  frameRate(0.5);
+
+  cp = createColorPicker("#FF0000")
+  cp.position(100,100)
 }
+
+function isRedSimilar(redA, greenA, blueA, redB, greenB, blueB){
+  let RED_SIMILARITY_VALUE = 60;
+  let OTHER_SIMILARITY_VALUE = 100;
+  if ( abs(redA - redB) < RED_SIMILARITY_VALUE && abs(blueA - blueB) < OTHER_SIMILARITY_VALUE && abs(greenA - greenB) < OTHER_SIMILARITY_VALUE) {
+    return true
+  }
+  return false
+}
+
+function isSimilar(redA, greenA, blueA, redB, greenB, blueB){
+  let SIMILARITY_VALUE = 100;
+  if ( abs(redA - redB) < SIMILARITY_VALUE && abs(blueA - blueB) < SIMILARITY_VALUE && abs(greenA - greenB) < SIMILARITY_VALUE) {
+    return true
+  }
+  return false
+}
+
 
 function draw() {
   background("white");
   noStroke();
-
+  mImg = originalImg.get()
   mImg.loadPixels();
-
-  let spacing = 32;
+  
+  let spacing = 1
 
   for (let y = 0; y < mImg.height; y += spacing) {
     for (let x = 0; x < mImg.width; x += spacing) {
@@ -26,21 +50,16 @@ function draw() {
       let greenVal = mImg.pixels[pixelIndex + 1];
       let blueVal = mImg.pixels[pixelIndex + 2];
 
-      if (redVal > 150) {
-        mImg.pixels[pixelIndex] = 255; // Set red component to max
-        mImg.pixels[pixelIndex + 1] = 0; // Green component remains 0
-        mImg.pixels[pixelIndex + 2] = 255; // Set blue component to max
+      
+      
+      if (isSimilar(redVal, greenVal, blueVal, red(cp.color()), green(cp.color()), blue(cp.color()))) {
+        mImg.pixels[pixelIndex] = 0; // Set red component to slider
+        mImg.pixels[pixelIndex + 1] = 255; // Set green component
+        mImg.pixels[pixelIndex + 2] = 0; // Set blue component
       }
-      if (greenVal < 100) {
-        mImg.pixels[pixelIndex + 1] = 128; // Set green component to a different value
-      }
-      if (blueVal < 100) {
-        mImg.pixels[pixelIndex + 2] = 128; // Set blue component to a different value
-      }
+
     }
   }
-
-  
 
   mImg.updatePixels();
   image(mImg, (width - mImg.width) / 2, 0);
